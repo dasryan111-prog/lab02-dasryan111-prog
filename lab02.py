c48e56c511cb1da38e66ab693a31028e228b007e
unknown <dasryan111@gmail.com>
2026-09-15 17:49:31 -0400

    Lab 02 Completed

--- lab02.py
+++ lab02.py
@@ -9,24 +9,71 @@
 def seconds_to_hms(total_seconds):
     # TODO (Part 1): return the time as a string "H:MM:SS"
     #   e.g. seconds_to_hms(3661) should return "1:01:01"
-    pass
+
+    hours = total_seconds // 3600
+    time_remaining = total_seconds - (hours * 3600)
+    minutes = time_remaining // 60
+    time_remaining = time_remaining -(minutes * 60)
+    seconds = time_remaining
+    time = f"{hours}:{minutes:02d}:{seconds:02d}"
+    return time
 
 
 def admission_price(age):
     # TODO (Part 2): return the ticket price (a number) for someone of this age
-    pass
+    if age < 5:
+        price = "0"
+    elif age > 4 and age < 13:
+        price = "8"
+    elif age > 12 and age < 65:
+        price = "12"
+    else:
+        price = "10"
+    return price
+
 
 
 def sum_multiples(limit):
     # TODO (Part 3): return the sum of every whole number below `limit`
     #   that is a multiple of 3 or of 5
-    pass
+    
+    # Multiples of 3
+    count_3 = 0
+    for i in range(limit):
+        if i % 3 == 0 and i != 0:
+            count_3 += 1
+
+    sum_multiples_3 = 0
+    for i in range (count_3):
+        current_multiple_3 = (i * 3) + 3
+        sum_multiples_3 += current_multiple_3
+
+    # Multiples of 5
+    count_5 = 0
+    for i in range(limit):
+        if i % 5 == 0 and i != 0:
+            count_5 += 1
+
+    sum_multiples_5 = 0
+    for i in range (count_5):
+        current_multiple_5 = (i * 5) + 5
+        if current_multiple_5 % 3 != 0:
+            sum_multiples_5 += current_multiple_5
+
+    return sum_multiples_3 + sum_multiples_5
 
 
 def total_of_positives(numbers):
     # TODO (Part 4 - STRETCH, optional): return the sum of just the
     #   positive numbers in the list `numbers`
     pass
+    sum_pos = 0
+    for i in range(len(numbers)):
+        if int(numbers[i]) > 0:
+            current_pos = int(numbers[i])
+            sum_pos += current_pos
+    return sum_pos
+
 
 
 def main():
@@ -36,6 +83,10 @@ def main():
     # print(admission_price(10))             # 8
     # print(sum_multiples(10))               # 23
     # print(total_of_positives([1, -2, 3]))  # 4
+    print(seconds_to_hms(3661))
+    print(admission_price(10))
+    print(sum_multiples(10))
+    print(total_of_positives([1,-2,3]))
     pass
 
 
